4169a94a49ce6bbf3f0f4ea5209c2b6d5482d65c
benax <benax@benax.com>
2014-06-23 16:05:26 +0200

    V1.1
    - Suppression du paramètre API_Version :
    J'ai fait un mix entre ZAPI1 et ZAPI2.
    Pour la génération de la grammaire et pour l'interrogation des sondes,
    j'utilise l'API2.
    Pour l'execution de commande, je suis resté sur l'API1 qui permet
    notamment de Dimmer des lampes, ce que l'API2 ne permet plus.
    Par ailleurs, l'ensemble des execution de commande (les
    "allume","eteint","ouvre"...) se fait en local.
    Il faut donc que la Zibase soit sur le même lan que SARAH.
    
    - Amélioration du générateur de grammaire : Ajout des articles et
    préposition sur les périphériques les plus courants.
    Par exemple, si un périphérique est nommé "lumiere salon" ou "radiateur
    chambre", les commandes automatiquement ajoutée par le plugin seront
    "allume la lumière du salon", ou "allume le radiateur de la chambre".
    De la même manière pour les sondes. Je recommande d'ailleurs de nommer
    vos sondes "temp XX" pour les sondes temp-hydro et "light xx" pour les
    sonde de luminosité.
    Ainsi si vous avez une sonde "temp Cuisine", la commande générée
    automatiquement sera "Comment est la température dans la cuisine".
    Il ne vous restera normalement plus grand chose à modifier dans votre
    grammaire.
    
    Petit conseil : une fois les périphériques importés et la grammaire
    ajustée à votre goût, faites une sauvegarde de votre ficher "xibase.xml"
    ou supprimez la ligne "mets a jour la domotique"... cela peut vous
    éviter de mauvaises surprises.

--- xibase.js
+++ xibase.js
@@ -21,28 +21,25 @@ exports.action = function(data, callback, config, SARAH){
 /**** VERIF DES PARAMETRES ***/
 console.log("xibase");
 
-if (!config.API_Version || !config.ip_lan || !config.device || !config.token || !config.plateforme_web) {
-		console.log("Les paramètres Xibase ne sont pas complets.");
+if (!config.ip_lan || !config.device || !config.token || !config.plateforme_web) {
+		callback({'tts': "Les paramètres Xibase ne sont pas complets."});
 		return;
 }
 /*****************************/
 /***** ETUDE DES ACTIONS *****/
 /*****************************/
+	var api2_path='https://'+ config.plateforme_web + '/api/get/ZAPI.php?zibase=' + config.device + '&token=' + config.token;
+
 
 /**** MISE A JOUR AUTOMATIQUE DES PERIHPERIQUES ****/
 if (data.actionModule == "UPDATE") {
-	if (!config.API_Version) {
-		console.log("Api_version missing");
-		return;
-		}
-	
-	if (config.API_Version == 1) {
-		console.log("Api_version 1");
-		device_URL="http://" + config.plateforme_web + "/m/get_xml.php?device=" + config.device + "&token=" + config.token;
-	}
+		
+		console.log("Api_version 2");
+		device_URL=api2_path + "&service=get&target=home";
+
  	// Send Request
 	var request = require('request');
-	request({ 'uri' : device_URL }, function (err, response, body){
+	request({ 'uri' : device_URL, 'json' : true }, function (err, response, body){
 		var tts=data.ttsAction;
 
 	// Retour uniquement en config web
@@ -52,67 +49,91 @@ if (data.actionModule == "UPDATE") {
 			return;
 		}
 	}
-	
-	var results = new xmldoc.XmlDocument(body);
 		var file = __dirname + '/xibase.xml';
 		var xml  = fs.readFileSync(file,'utf8');
 		var replace  = '§ -->\n';
-		/****** recuperation des modules ***/
-		var evs = results.childrenNamed("e");
-	//	var ev = evs.childWithAttribute('id',module.attr.addr);
-	//	var ttsEnd = "";
 	var tts = "LEs paramètres on bien été importés. Voici les commandes possible. ";
-		
-		for (i = 0; i < evs.length; i++) { 
-			var t=evs[i].attr.t;
-			if (t=="COMMANDE2") {
-					var typeaction="telecommande";
-					var adresse1=evs[i].attr.c1;
-					var adresse2=evs[i].attr.c2;
-					var nom=evs[i].childNamed("n").val;
-					tts += nom + " qui est une télécommande.  "
-					 replace += '      <item>'+ nom +'<tag>out.action.ttsName="'+nom+'";out.action.typeAction="'+typeaction+'";out.action.bouton1="'+adresse1+'";out.action.bouton2="'+adresse2+'";</tag></item>\n';
-			}
-			if (t=="receiverXDom") {
-					var typeaction="actionneur";
-					var adresse=evs[i].attr.c;
-					var protocol=evs[i].attr.p;
-					var nom=evs[i].childNamed("n").val;
-					tts += nom + " qui est un actionneur.  "
-					 replace += '      <item>'+ nom +'<tag>out.action.ttsName="'+nom+'";out.action.typeAction="'+typeaction+'";out.action.adresse="'+adresse+'";out.action.protocol="'+protocol+'";</tag></item>\n';
-			}
-			if (t=="temperature") {
-					var typeaction="sonde";
-					var typesonde="temperature";
-					var adresse=evs[i].attr.c;
-					var nom="la température dans " + evs[i].childNamed("n").val;
-					tts += nom + " qui est une sonde de température.  "
-					 replace += '      <item>'+ nom +'<tag>out.action.ttsName="'+evs[i].childNamed("n").val+'";out.action.typeSonde="'+typesonde+'";out.action.typeAction="'+typeaction+'";out.action.adresse="'+adresse.replace('OS','')+'";</tag></item>\n';
-			}
-			if (t=="light") {
-					var typeaction="sonde";
-					var typesonde="luminosite";
-					var adresse=evs[i].attr.c;
-					var nom="la luminosité dans " + evs[i].childNamed("n").val;
-					tts += nom + " qui est une sonde de luminosité.  "
-					 replace += '      <item>'+ nom +'<tag>out.action.ttsName="'+evs[i].childNamed("n").val+'";out.action.typeSonde="'+typesonde+'";out.action.typeAction="'+typeaction+'";out.action.adresse="'+adresse.replace('OS','')+'";</tag></item>\n';
-			}
-		}
-		/**** recuperation des scenarios ***/
-		var evs = results.childrenNamed("m");
-		
-		for (i = 0; i < evs.length; i++) { 
-			var id=evs[i].attr.id;
-			var nom=evs[i].childNamed("n").val;
+
+		/***** actionneurs ***/
+	var results=body.body.actuators;
+	for (i = 0; i < results.length; i++) {
+			var typeaction="actionneur";
+			var adresse=results[i].id;
+			var protocol=results[i].protocol;
+			var nom=results[i].name;
+			/********* ajour des articles pour les peripheriques le splus connus*****/
+			var commande=replaceAll(nom,"lumiere","lumière");
+			commande=replaceAll(commande,"lumière","la lumière");
+			commande=replaceAll(commande,"lampe","la lampe");
+			commande=replaceAll(commande,"volet","le volet");
+			commande=replaceAll(commande,"radiateur","le radiateur");
+			commande=replaceAll(commande,"chauffage","le chauffage");
+			commande=replaceAll(commande,"alarme","l'alarme");
+			commande=replaceAll(commande,"portail","le portail");
+			commande=replaceAll(commande,"chevet","le chevet");
+			commande=replaceAll(commande,"plafonnier","le plafonnier");
+			commande=replaceAll(commande,"VMC","la VMC");
+			commande=replaceAll(commande,"ventilation","la ventilation");
+			commande=replaceAll(commande,"chaudiere","chaudière");
+			commande=replaceAll(commande,"chaudière","la chaudière");
+			commande=replaceAll(commande,"sdb","salle de bain");
+			commande=preposition(commande);
+			tts += commande + " qui est un actionneur.  "
+			
+			 replace += '      <item>'+ commande +'<tag>out.action.ttsName="'+commande+'";out.action.typeAction="'+typeaction+'";out.action.adresse="'+adresse+'";out.action.protocol="'+protocol+'";</tag></item>\n';
+	}
+		/***** telecommandes ***/
+	var results=body.body.remotes;
+	for (i = 0; i < results.length; i++) {
+			var typeaction="telecommande";
+			var adresse1=results[i].idON;
+			var adresse2=results[i].idOFF;
+			var nom=results[i].name;
+			tts += nom + " qui est une télécommande.  "
+			 replace += '      <item>'+ nom +'<tag>out.action.ttsName="'+nom+'";out.action.typeAction="'+typeaction+'";out.action.bouton1="'+adresse1+'";out.action.bouton2="'+adresse2+'";</tag></item>\n';
+	}
+		/***** sondes ***/
+	var results=body.body.probes;
+	for (i = 0; i < results.length; i++) {
+		if (results[i].type=="temperature") {
+				var typeaction="sonde";
+				var typesonde="temperature";
+				var adresse=results[i].id;
+				var nom="la température dans " + results[i].name;
+				nom=article_sonde(nom);
+				tts += nom + " qui est une sonde de température.  "
+				 replace += '      <item>'+ nom +'<tag>out.action.ttsName="'+results[i].name+'";out.action.typeSonde="'+typesonde+'";out.action.typeAction="'+typeaction+'";out.action.adresse="'+adresse+'";</tag></item>\n';
+				var typeaction="sonde";
+				var typesonde="hydro";
+				var adresse=results[i].id;
+				var nom="l'hygrométrie dans " + results[i].name;
+				nom=article_sonde(nom);
+				tts += nom + " qui est une sonde de d'hygrométrie.  "
+				 replace += '      <item>'+ nom +'<tag>out.action.ttsName="'+results[i].name+'";out.action.typeSonde="'+typesonde+'";out.action.typeAction="'+typeaction+'";out.action.adresse="'+adresse+'";</tag></item>\n';
+			 }
+		if (results[i].type=="light") {
+				var typeaction="sonde";
+				var typesonde="luminosite";
+				var adresse=results[i].id;
+				var nom="la luminosité dans " + results[i].name;
+				nom=article_sonde(nom);
+				tts += nom + " qui est une sonde de luminosité.  "
+				 replace += '      <item>'+ nom +'<tag>out.action.ttsName="'+results[i].name+'";out.action.typeSonde="'+typesonde+'";out.action.typeAction="'+typeaction+'";out.action.adresse="'+adresse+'";</tag></item>\n';
+			 }
+	}
+		/***** scenarios ***/
+	var results=body.body.scenarios;
+	for (i = 0; i < results.length; i++) {
+			var id=results[i].id;
+			var nom=results[i].name;
 					tts += nom + " qui est un scénario.  "
 			replace += '      <item>'+ nom +'<tag>out.action.ttsName="'+nom+'";out.action.typeAction="MACRO";out.action.idMacro="'+id+'";</tag></item>\n';
-		}
-
-		 replace += '<!-- §';      
+	}
+			 replace += '<!-- §';   
+		 
   var regexp = new RegExp('§[^§]+§','gm');
   var xml    = xml.replace(regexp,replace);
   fs.writeFileSync(file, xml, 'utf8');
-	console.log("tts " + tts);
 	callback({'tts': tts});
 
 	});
@@ -165,7 +186,8 @@ else if (data.actionModule == "SONDE") {
 	if (data.typeAction != "sonde") {
 		callback({'tts': "ceci n'est pas une sonde"});
 		return;
-	}			url = http+config.ip_lan+sensors_path;
+	}			
+	url = api2_path + "&service=get&target=probe&id=" + data.adresse;
 }
 
 		console.log("Sending request to: " + url);
@@ -180,7 +202,7 @@ else if (data.actionModule == "SONDE") {
   
  	// Send Request
 	var request = require('request');
-	request({ 'uri' : url }, function (err, response, body){
+	request({ 'uri' : url, 'json' : true  }, function (err, response, body){
 	
 	// Retour uniquement en config web
 	if(config.acces_method == "web") {
@@ -190,32 +212,31 @@ else if (data.actionModule == "SONDE") {
 		}
 	}
 	
-	// Sensors action => Parsing XML
+	// Sensors action => Parsing JSON
 	if (data.typeAction == 'sonde') {
-		var results = new xmldoc.XmlDocument(body);
-		var evs = results.childNamed("evs");
-		var ev = evs.childWithAttribute('id',data.adresse);
-		console.log("sonde : " + ev);
 		var ttsEnd = "";
 		var sonde;
 		if (data.typeSonde == 'luminosite') {
-			var sonde = ev.attr.v1;
+			var sonde = body.body.val1;
 			tts = "La luminosité de " + data.ttsName + " est de %s lux";
 
 		}
 		if (data.typeSonde == 'temperature') {
-			var sonde = ev.attr.v1;
-			sonde = RoundTo2Decimals(parseFloat(sonde)/10);
+			var sonde = body.body.val1;
+			console.log("sonde : " + sonde);
+			//sonde = RoundTo2Decimals(parseFloat(sonde)/10);
 			tts = "La température de " + data.ttsName + " est de %s degrés";
+
+		}	
+		if (data.typeSonde == 'hydro') {
+			var sonde = body.body.val2;
+			tts = "L'hygrométrie de " + data.ttsName + " est de %s pourcents";
 
 		}	
 		
 		ttsEnd = parseFloat(sonde).toString().replace("."," virgule ");
-		console.log("valeur : " + sonde);
 		
-//		tts = "La température de " + data.ttsName + " est de %s degrés";
 		tts = tts.replace('%s', ttsEnd);
-		console.log("valeur : " + ttsEnd);
 	}
 	
     else {
@@ -223,7 +244,6 @@ else if (data.actionModule == "SONDE") {
 		if (data.ttsDim){
 			tts += " " + data.ttsDim;
 		}
-		console.log("oups");
 	}
 	// Callback with TTS
 	callback({'tts': tts});
@@ -241,4 +261,34 @@ function FormatNumberLength(num, length) {
 
 function RoundTo2Decimals(numberToRound) {
   return Math.round(numberToRound * 100) / 100
+}
+
+function preposition(nom) {
+	var result=replaceAll(nom,"cuisine","de la cuisine");
+	result=replaceAll(result,"salon","du salon");
+	result=replaceAll(result,"salle à manger","de la salle à manger");
+	result=replaceAll(result,"billard","du billard");
+	result=replaceAll(result,"salle de bain","de la salle de bain");
+	result=replaceAll(result,"chambre","de la chambre");
+	return result;
+	
+}
+
+function article_sonde(nom) {
+	var commande=replaceAll(nom,"temp ","");
+	commande=replaceAll(commande,"light ","");
+	commande=replaceAll(commande,"chambre","la chambre");
+	commande=replaceAll(commande,"salon","le salon");
+	commande=replaceAll(commande,"salle à manger","la salle à manger");
+	commande=replaceAll(commande,"salle a manger","la salle à manger");
+	commande=replaceAll(commande,"cuisine","la cuisine");
+	commande=replaceAll(commande,"SDB","salle de bain");
+	commande=replaceAll(commande,"salle de bain","la salle de bain");
+	return commande;
+	}
+	
+function replaceAll(origin,searchMask,replaceMask) {
+	var regEx = new RegExp(searchMask, "ig");
+	var result = origin.replace(regEx, replaceMask);
+	return result;
 }
